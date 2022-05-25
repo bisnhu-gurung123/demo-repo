8c59dbbbf0eb9d1cb79cc5202943f7ee724c4dbb
bishnu <bishnu.gurung@selise.ch>
2022-05-25 13:46:27 +0600

    landing page

--- demo-app/src/app/app.module.ts
+++ demo-app/src/app/app.module.ts
@@ -3,15 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from "@angular/material/card";
+import {MatChipsModule} from "@angular/material/chips";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatMenuModule} from "@angular/material/menu";
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {MatIconModule} from "@angular/material/icon";
+import {MatDividerModule} from "@angular/material/divider";
+import {MatButtonModule} from "@angular/material/button";
+import {MatInputModule} from "@angular/material/input";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatCardModule,
+        MatChipsModule,
+        MatPaginatorModule,
+        MatFormFieldModule,
+        MatMenuModule,
+        FlexLayoutModule,
+        MatIconModule,
+        MatDividerModule,
+        MatButtonModule,
+        MatInputModule,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
