181154877d616803c8680ecbba0f0db273c2fba0
bishnu <bishnu.gurung@selise.ch>
2022-05-26 13:40:53 +0600

    add card

--- demo-app/src/app/app.module.ts
+++ demo-app/src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatCardModule} from "@angular/material/card";
 import {MatChipsModule} from "@angular/material/chips";
 import {MatPaginatorModule} from "@angular/material/paginator";
@@ -14,27 +14,31 @@ import {MatIconModule} from "@angular/material/icon";
 import {MatDividerModule} from "@angular/material/divider";
 import {MatButtonModule} from "@angular/material/button";
 import {MatInputModule} from "@angular/material/input";
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    CardComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        BrowserAnimationsModule,
-        MatCardModule,
-        MatChipsModule,
-        MatPaginatorModule,
-        MatFormFieldModule,
-        MatMenuModule,
-        FlexLayoutModule,
-        MatIconModule,
-        MatDividerModule,
-        MatButtonModule,
-        MatInputModule,
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatChipsModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatMenuModule,
+    FlexLayoutModule,
+    MatIconModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatInputModule
+
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
